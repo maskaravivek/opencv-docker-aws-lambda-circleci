e4080c77ccf2e874abfd08ffb1181d59ba0907f8
Vivek Kumar Maskara <maskaravivek@gmail.com>
2024-04-14 14:22:24 -0700

    Minor updates

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # OpenCV AWS Lambda
 
+Build:
+
 ```
-docker run --platform linux/amd64 -e AWS_ACCESS_KEY_ID='AKIA5QAHTOTIKVFAEH3V' \
--e AWS_SECRET_ACCESS_KEY='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' \
+docker build --platform linux/amd64 -t opencv-grayscale-image:1.0.0 .
+```
+
+Tag:
+
+```
+docker tag opencv-grayscale-image:1.0.0 927728891088.dkr.ecr.us-west-2.amazonaws.com/opencv-grayscale-image:1.0.0
+```
+
+Push:
+
+```
+docker push 927728891088.dkr.ecr.us-west-2.amazonaws.com/opencv-grayscale-image:1.0.0
+```
+
+Run locally:
+
+```
+docker run --platform linux/amd64 -e AWS_ACCESS_KEY_ID='<YOUR_KEY>' \
+-e AWS_SECRET_ACCESS_KEY='<YOUR_ACCESS_KEY>' \
 -e AWS_DEFAULT_REGION='us-west-2' \
 --env-file .env \
--p 9000:8080 docker-image:test
+-p 9000:8080 opencv-grayscale-image:1.0.0
 ```
 
 ```
-curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"body": "{\"imageUrl\":\"https://picsum.photos/id/237/536/354\"}" }'
 ```
--- app.py
+++ app.py
@@ -3,25 +3,35 @@ from process_task import convert_image_to_grayscale
 import os
 
 def lambda_handler(event, context):
-    request_body = json.loads(event['body'])
+    try:
+        request_body = json.loads(event['body'])
     
-    image_url = request_body['imageUrl']
+        image_url = request_body['imageUrl']
+        
+        gray_image_path = convert_image_to_grayscale(image_url)
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'grayImagePath': gray_image_path
+            })
+        }   
     
-    gray_image_path = convert_image_to_grayscale(image_url)
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'grayImagePath': gray_image_path
-        })
-    }   
+    except Exception as e:
+        print("Error: ", e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': str(e)
+            })
+        }
 
 if __name__ == "__main__":
     import os
     
     event = {
         'body': json.dumps({
-            'imageUrl': 'https://i.imgur.com/afKn8xK.jpg'
+            'imageUrl': 'https://picsum.photos/id/237/536/354'
         })
     }
     
